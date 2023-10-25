aa19ca36236a606d02566ae98e3ae05c46f93a8f
Miguel <miggi92@users.noreply.github.com>
2023-10-25 22:11:30 +0000

    added i18n config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,15 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxt/ui", "@nuxt/image", "@nuxtjs/i18n"],
+  i18n: {
+    lazy: true,
+    defaultLocale: "en",
+    strategy: "no_prefix",
+    langDir: "locales",
+    locales: [
+      { code: "de", file: "de.json" },
+      { code: "en", file: "en.json" },
+    ],
+    vueI18n: "./i18n.config.ts",
+  },
 })
