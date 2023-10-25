e17ae1f20d6f7a2111e92f1da3bd7607cc546210
Miguel <miggi92@users.noreply.github.com>
2023-10-25 23:05:11 +0000

    added title to about page

--- pages/about.vue
+++ pages/about.vue
@@ -1,1 +1,9 @@
-<template></template>
+<template>
+    <h1 class="text-primary text-5xl">{{ t('clubName') }}</h1>
+</template>
+
+<script setup lang="ts">
+const { t } = useI18n({
+    useScope: 'local'
+})
+</script>
