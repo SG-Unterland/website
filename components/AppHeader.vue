95ddebbf2857c1200f0c680a29075d000499a464
Miguel <miggi92@users.noreply.github.com>
2023-11-07 12:39:46 +0000

    updated header component

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,40 +1,41 @@
 
 <template>
-    <nav class="bg-white">
-        <div class="max-w-6xl mx-auto px-4">
-            <div class="flex justify-between">
-                <div class="flex space-x-7">
-                    <div class="flex items-center py-4 px-2">
-                        <NuxtImg src="/logo.png" width="20" height="20" class="mr-2" />
-                        <NuxtLink to="/" class="text-primary">{{ t('clubName') }}</NuxtLink>
-                    </div>
-                    <div class="md:flex items-center space-x-1 text-black" v-for="navItem in navItems" :key="navItem.id">
-                        <NuxtLink class="py-4 px-2" :to="navItem.path" active-class="text-primary">{{
-                            t(navItem.id) }}
-                        </NuxtLink>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </nav>
+    <UHeader :links="links">
+        <template #logo>
+            <AppLogo />
+        </template>
+
+        <template #right>
+            <UDocsSearchButton :label="null" />
+
+            <UColorModeButton />
+        </template>
+
+        <template #panel>
+            <UNavigationTree :links="mapContentNavigation(navigation)" />
+        </template>
+    </UHeader>
 </template>
 
 <script setup lang="ts">
+import type { NavItem } from '@nuxt/content/dist/runtime/types'
+const navigation = inject<Ref<NavItem[]>>('navigation')
+
 const { t } = useI18n({
     useScope: 'local'
 })
-const navItems = [
+const links = [
     {
-        path: '/',
-        id: 'home'
+        to: '/',
+        label: t('home'),
     },
     {
-        path: '/about',
-        id: 'about'
+        to: '/about',
+        label: t('about')
     },
     {
-        path: '/contact',
-        id: 'contact'
+        to: '/contact',
+        label: t('contact')
     }
 ]
 </script>
---
+++ components/AppLogo.vue
@@ -0,0 +1,3 @@
+<template>
+    <NuxtImg src="/logo.png" width="20" height="20" class="mr-2" />
+</template>
