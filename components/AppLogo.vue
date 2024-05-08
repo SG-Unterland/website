139bb90c3c01b515c0b60ea9ba6a52eceaa6d741
Miguel <miggi92@users.noreply.github.com>
2024-05-08 10:26:21 +0000

    feat: :sparkles: added logo to headerbar

--- components/AppLogo.vue
+++ components/AppLogo.vue
@@ -1,4 +1,3 @@
 <template>
-    <UIcon name="i-mdi-handball" />
-    <!-- <NuxtImg src="/logo.png" width="20" height="20" class="mr-2" /> -->
+    <NuxtImg src="/logo.jpg" width="20" height="20" class="mr-2" /> 
 </template>
